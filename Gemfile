b61133c377a4a0fe7643402711eb5d7301c61f0f
Joel Turkel <jturkel@salsify.com>
2014-02-21 12:27:47 -0500

    Fix code coverage reporting.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ group :test do
   gem 'delayed_job', '>= 3.0'
   gem 'delayed_job_active_record', '>= 0.4'
   gem 'rspec', '>= 2.14'
+  # See https://github.com/colszowka/simplecov/issues/281 for why we need 0.7.1
+  gem 'simplecov', '~> 0.7.1', require: false
 
   platforms :ruby do
     gem 'sqlite3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+ add_filter 'spec'
+end
 
 require 'database_cleaner'
 require 'delayed_job_groups'
