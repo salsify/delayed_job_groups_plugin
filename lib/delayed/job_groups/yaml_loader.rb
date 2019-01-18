47799ebdfef1c5db50c9e23f2ee90a049d29d880
Kevin Yang <kyang@salsify.com>
2019-01-18 09:46:05 -0500

    match

--- lib/delayed/job_groups/yaml_loader.rb
+++ lib/delayed/job_groups/yaml_loader.rb
@@ -4,7 +4,7 @@ module Delayed
   module JobGroups
     module YamlLoader
       def self.load(yaml)
-        return yaml unless yaml.is_a?(String) && /^---/.match?(yaml)
+        return yaml unless yaml.is_a?(String) && /^---/.match(yaml)
         YAML.load_dj(yaml)
       end
 
