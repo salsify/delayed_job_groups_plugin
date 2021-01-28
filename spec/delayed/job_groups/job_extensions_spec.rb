6ec6f1b9fa8bbb7d91c01fbd375c34947f681382
Kyle Phelps <kphelps@salsify.com>
2021-01-28 12:41:25 -0500

    Fix belongs_to with belongs_to_required_by_default

---
+++ spec/delayed/job_groups/job_extensions_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+describe "delayed job extensions" do
+  it "provides an optional job_group_id" do
+    job_group = Delayed::JobGroups::JobGroup.create!
+    expect(Delayed::Job.new(job_group_id: job_group.id)).to be_valid
+    expect(Delayed::Job.new).to be_valid
+  end
+end
