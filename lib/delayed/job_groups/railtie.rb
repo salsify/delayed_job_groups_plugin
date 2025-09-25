d7dd81ad00bbb0b604da5d9c3defc72014d471f2
Marco <mserrato@salsify.com>
2025-09-25 11:38:50 -0400

    Revert "Move On Cancellation Logic to After Failure Hook"
    This reverts commits
    - 2ee55d54a067aafa9e4f938592558598286a9c13
    - bf9bc686b9f346424ebc3ac1aef344267f0485e1
    - a6f9528d6d03fe94d09c99bc3e2cf8735dff2c49
    - 525a00dd7369de996efd102dcfb6b32c61a88794

--- lib/delayed/job_groups/railtie.rb
+++ lib/delayed/job_groups/railtie.rb
@@ -4,15 +4,6 @@ module Delayed
   module JobGroups
     class Railtie < ::Rails::Railtie
       config.after_initialize do
-
-        # On cancellation checks are performed in the after failure delayed job lifecycle, however
-        # https://github.com/collectiveidea/delayed_job/blob/master/lib/delayed/worker.rb#L268 may
-        # delete jobs before the hook runs. This could cause a successful job in the same group to
-        # complete the group instead of the group being cancelled. Therefore, we must ensure that
-        # the Delayed::Worker.destroy_failed_jobs is set to false, guaranteeing that the group is
-        # never empty if failure occurs.
-        raise Delayed::JobGroups::IncompatibleWithDelayedJobError if Delayed::Worker.destroy_failed_jobs
-
         Delayed::Backend::ActiveRecord::Job.include(Delayed::JobGroups::JobExtensions)
       end
     end
